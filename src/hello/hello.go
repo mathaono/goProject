b4cbf0bb57ec42d8c342bcd7b7c2330e89d4ebb9
mataonodeo <matheus.oliveira@engdb.com.br>
2022-01-28 16:25:24 -0300

    As principais coleções de Go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramento = 3
+const delay = 3
 
 func main() {
 
@@ -58,12 +62,26 @@ func showAction() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando")
-	site := "https://www.alura.com.br"
-	resp, _ := http.Get(site)
+	sites := []string{"https://www.alura.com.br",
+		"https://www.caelum.com.br",
+		"https://www.facebook.com",
+		"https://www.olhardigital.com.br"}
 
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i)
+			testaSites(site)
+		}
+		fmt.Println("")
+		time.Sleep(delay * time.Second)
+	}
+}
+
+func testaSites(site string) {
+	resp, _ := http.Get(site)
 	if resp.StatusCode == 200 {
 		fmt.Println("Site: ", site, "carregado com sucesso!")
 	} else {
-		fmt.Println("Site: ", site, "com problemas!")
+		fmt.Println("Site: ", site, "com problemas! Status Code:", resp.StatusCode)
 	}
 }
